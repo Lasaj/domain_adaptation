5064069146afc639cecd17dbba4634deb4ab50eb
Lasaj <rick.wainwright2@gmail.com>
2023-07-11 09:57:25 +1000

    add early stopping and save model weights

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torchvision
 import torch
 import wandb
@@ -6,6 +7,7 @@ import time
 import covidx
 from torchvision.models import DenseNet, DenseNet121_Weights
 
+start_time = time.strftime('%Y%m%d_%H%M')
 
 train_dl, test_dl = covidx.get_source_covidx()
 
@@ -52,7 +54,7 @@ def start_logging(model, epochs, model_name, train_batch_size, run_name):
                        "model": model_name,
                        "batch_size": train_batch_size})
 
-    wandb.run.name = f"{time.strftime('%Y%m%d_%H%M')}_{run_name}"
+    wandb.run.name = f"{start_time}_{run_name}"
     wandb.watch(model)
 
 
@@ -101,13 +103,25 @@ def test(dataloader, model, classifier, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     wandb.log({"test loss": test_loss, "accuracy": 100 * correct})
+    return test_loss
 
 
 epochs = 100
+best_loss = np.Inf
+patience = 10
+
 for t in range(epochs):
     print(f"Epoch {t + 1}\n-------------------------------")
     train(train_dl, model, classifier, loss_fn, optimizer)
-    test(test_dl, model, classifier, loss_fn)
+    test_loss = test(test_dl, model, classifier, loss_fn)
+    # early stopping
+    if test_loss < best_loss:
+        best_loss = test_loss
+        model.save(f"{start_time}_covidx_densenet.pt")
+    else:
+        patience -= 1
+        if patience == 0:
+            break
 print("Done!")
 
 classes = ["covid", "normal", "pneumonia"]

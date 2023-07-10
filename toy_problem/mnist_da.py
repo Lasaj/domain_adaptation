b513906d9231c34ed1579b171e880f4ccddb1410
Lasaj <rick.wainwright2@gmail.com>
2023-07-10 16:11:01 +1000

    small classifier

--- toy_problem/mnist_da.py
+++ toy_problem/mnist_da.py
@@ -54,11 +54,12 @@ class Classifier(nn.Module):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(1024, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
+            # nn.Linear(1024, 512),
+            # nn.ReLU(),
+            # nn.Linear(512, 512),
+            # nn.ReLU(),
+            # nn.Linear(512, 10)
+            nn.Linear(1024, 10),
         )
 
     def forward(self, x):
